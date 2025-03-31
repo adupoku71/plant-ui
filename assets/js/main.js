7a68a296590e6d8654d506ec76bb35670d44deb2
adupoku71 <davipoku71@gmail.com>
2025-03-31 18:43:27 +0000

    refactored toggling into a single function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,16 @@
 // TOGGLE BUTTON
 
-const navMenu = document.getElementById("nav-menu")
-const navLinks = document.querySelectorAll(".nav-link")
-const hamburger = document.getElementById("hamburger")
+document.addEventListener("DOMContentLoaded", () => {
+  const navMenu = document.getElementById("nav-menu")
+  const hamburger = document.getElementById("hamburger")
 
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("left-[0]")
-  hamburger.classList.toggle("ri-close-large-line")
-})
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
+  const toggleMenu = () => {
     navMenu.classList.toggle("left-[0]")
     hamburger.classList.toggle("ri-close-large-line")
-  })
+  }
+
+  hamburger.addEventListener("click", toggleMenu)
+  document
+    .querySelectorAll(".nav-link")
+    .forEach((link) => link.addEventListener("click", toggleMenu))
 })
