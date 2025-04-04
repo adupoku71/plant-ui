a32bd33ab9cf4eb81c3604d86ee5838d716ed5ff
adupoku71 <davipoku71@gmail.com>
2025-04-04 23:15:42 +0000

    sticky filter section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const filterDropDown = document.querySelectorAll(".filter-dropdown")
+
   // Filter Section Drop Down
   document.querySelectorAll(".filter-trigger").forEach((trigger) => {
     trigger.addEventListener("click", (e) => {
       e.stopPropagation()
+      console.log("clicked")
+
       const dropdown = trigger.nextElementSibling
       console.log(dropdown)
 
-      dropdown.classList.toggle("hidden")
+      if (dropdown) {
+        const height = `h-[${dropdown.scrollHeight}px]`
 
-      document.querySelectorAll(".filter-dropdown").forEach((d) => {
-        if (d !== dropdown) d.classList.add("hidden")
-      })
+        dropdown.classList.toggle(height)
+
+        // Toggle dropdown height
+        filterDropDown.forEach((d) => {
+          if (d !== dropdown) {
+            d.classList.remove(height)
+          }
+        })
+      }
+      //   const icon = trigger.querySelector("i")
     })
   })
 
   document.addEventListener("click", () => {
     document.querySelectorAll(".filter-dropdown").forEach((d) => {
-      d.classList.add("hidden")
+      d.classList.remove("h-[160px]")
     })
   })
 
   // Sticky FilterSEction
   const filterSection = document.getElementById("filter-section")
-  const navbar = document.querySelector("#mobile-nav")
-  const navbarHeight = navbar.offsetHeight
+  //   const navbar = document.querySelector("#mobile-nav")
+  //   const navbarHeight = navbar.offsetHeight
+  const navbarHeight = 64
   const originalOffsetTop = filterSection.offsetTop
 
   window.addEventListener("scroll", () => {
@@ -34,9 +47,11 @@ document.addEventListener("DOMContentLoaded", () => {
         "top-14",
         "w-full",
         "z-50",
-        "bg-white",
+        "bg-gray-50",
         "shadow-sm",
-        "border-t"
+        "left-0",
+        "bg-white"
+        // "border-t"
         //   "border-gray-200"
       )
       filterSection.classList.remove("relative", "border-b", "py-3")
@@ -54,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "shadow-sm",
         "border-t"
       )
-      filterSection.classList.add("relative", "border-b", "py-3")
+      filterSection.classList.add("relative", "py-3")
       // document.querySelectorAll(".filter-trigger").forEach((filter) => {
       //   filter.classList.add("border-b-2", "border-gray-300")
       // })
