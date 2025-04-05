4b765a960573634e1749ba5ab3850126d3beb0bf
adupoku71 <davipoku71@gmail.com>
2025-04-05 10:26:29 +0000

    fixed navbar and changed icons

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,10 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".filter-trigger").forEach((trigger) => {
     trigger.addEventListener("click", (e) => {
       e.stopPropagation()
-      console.log("clicked")
-
+      document.querySelectorAll(".filter-trigger").forEach((t) => {
+        t.classList.remove("border-b-2")
+      })
+      trigger.classList.toggle("border-b-2")
       const dropdown = trigger.nextElementSibling
-      console.log(dropdown)
 
       if (dropdown) {
         const height = `h-[${dropdown.scrollHeight}px]`
@@ -27,6 +28,12 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 
   document.addEventListener("click", () => {
+    document.querySelectorAll(".filter-trigger").forEach((d) => {
+      d.classList.remove("border-b-2")
+      d.classList.remove("h-[160px]")
+    })
+  })
+  document.addEventListener("click", () => {
     document.querySelectorAll(".filter-dropdown").forEach((d) => {
       d.classList.remove("h-[160px]")
     })
@@ -34,9 +41,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Sticky FilterSEction
   const filterSection = document.getElementById("filter-section")
-  //   const navbar = document.querySelector("#mobile-nav")
-  //   const navbarHeight = navbar.offsetHeight
-  const navbarHeight = 64
+  const navbar = document.querySelector("#mobile-nav")
+  const navbarHeight = navbar.offsetHeight
+  //   const navbarHeight = 60
   const originalOffsetTop = filterSection.offsetTop
 
   window.addEventListener("scroll", () => {
@@ -44,32 +51,35 @@ document.addEventListener("DOMContentLoaded", () => {
       // When sticky
       filterSection.classList.add(
         "fixed",
-        "top-14",
+        `top-[${navbarHeight}px]`,
         "w-full",
         "z-50",
         "bg-gray-50",
         "shadow-sm",
         "left-0",
         "bg-white"
+        // "pb-2"
+        // "bg-blue-700"
         // "border-t"
         //   "border-gray-200"
       )
-      filterSection.classList.remove("relative", "border-b", "py-3")
-      document.querySelectorAll(".filter-trigger").forEach((filter) => {
-        filter.classList.remove("border-b-2", "border-gray-300")
-      })
+      filterSection.classList.remove("relative", "border-b", "py-2")
+      //   document.querySelectorAll(".filter-trigger").forEach((filter) => {
+      //     filter.classList.remove("border-b-2", "border-gray-300")
+      //   })
     } else {
       // When normal
       filterSection.classList.remove(
         "fixed",
-        "top-14",
+        `top-[${navbarHeight}px]`,
         "w-full",
         "z-50",
         "bg-white",
         "shadow-sm",
-        "border-t"
+        "border-t",
+        "pb-2"
       )
-      filterSection.classList.add("relative", "py-3")
+      //   filterSection.classList.add("relative", "py-2")
       // document.querySelectorAll(".filter-trigger").forEach((filter) => {
       //   filter.classList.add("border-b-2", "border-gray-300")
       // })
